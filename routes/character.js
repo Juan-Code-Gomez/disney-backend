14fbc1cad347c7f59d2a713cbd03e455cc289d8e
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-04-24 11:32:53 -0500

    Se crea la ruta para filtrar los personajes por nombre, edad y pelicula.

--- routes/character.js
+++ routes/character.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { Op } = require("sequelize");
 const { Character, Movie } = require("../models");
 
 router.post("/character", async (req, res) => {
@@ -16,26 +17,68 @@ router.post("/character", async (req, res) => {
     });
 
     if (movieId) {
-        const movie = await Movie.findByPk(movieId);
+      const movie = await Movie.findByPk(movieId);
 
-        if (movie) {
-            await newCharacter.addMovie(movie, {
-              through: {
-                createdAt: new Date(),
-                updatedAt: new Date(), 
-              },
-            });
-          } else {
-            // Si la pelicula no se encuentra, devolver un error
-            return res.status(404).json({ error: "Pelicula no encontrada" });
-          }
+      if (movie) {
+        await newCharacter.addMovie(movie, {
+          through: {
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        });
+      } else {
+        // Si la pelicula no se encuentra, devolver un error
+        return res.status(404).json({ error: "Pelicula no encontrada" });
+      }
     }
 
     res.status(201).json(newCharacter);
-
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Error al crear el personaje" });
+  }
+});
+
+router.get("/character", async (req, res) => {
+  try {
+    const { name, age, movie } = req.query;
+
+    let conditions = [];
+
+    if (!name && !age && !movie) {
+      Character.findAll().then((character) => {
+        res.json(character);
+      });
+    } else {
+      if (name) {
+        conditions.push({ name: { [Op.like]: `%${name}%` } });
+      }
+
+      if (age) {
+        conditions.push({ age });
+      }
+
+      if (movie) {
+        conditions.push({ movie });
+      }
+
+      const characters = await Character.findAll({
+        where: {
+          [Op.or]: conditions,
+        },
+      });
+
+      if (characters.length > 0) {
+        res.json(characters);
+      } else {
+        res.send(
+          "No se encontraron personajes con los parámetros especificados."
+        );
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Ocurrió un error al realizar la consulta.");
   }
 });
 
