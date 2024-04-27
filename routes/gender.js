f5ab955092c5cbe9679e2b1ef0ce37de3f1ad2a0
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-04-26 22:44:09 -0500

    Cambios finales, arreglos de consulta y bugs.

--- routes/gender.js
+++ routes/gender.js
@@ -8,9 +8,14 @@ router.post("/gender", async (req, res) => {
   try {
     const { name, image, movie } = req.body;
 
-    if (name === '' || image === '' || name === undefined || image === undefined) {
-      console.log('entramos al if');
-      return res.status(400).json("Debes ingresar un nombre y una imagen")
+    if (
+      name === "" ||
+      image === "" ||
+      name === undefined ||
+      image === undefined
+    ) {
+      console.log("entramos al if");
+      return res.status(400).json("Debes ingresar un nombre y una imagen");
     }
 
     const newGender = await Gender.create({
@@ -31,34 +36,34 @@ router.get("/gender", async (req, res) => {
     const genderName = req.query.name;
 
     if (!genderId && !genderName) {
-      return res
-        .status(400)
-        .json({ error: "Debes proporcionar un ID o un nombre para el género" });
-    }
-
-    let gender;
-    if (genderId) {
-      gender = await Gender.findByPk(genderId, {
-        include: {
-          model: Movie,
-          through: { attributes: [] },
-        },
+      Gender.findAll().then((gender) => {
+        res.json(gender);
       });
-    } else if (genderName) {
-      gender = await Gender.findOne({
-        where: { name: genderName },
-        include: {
-          model: Movie,
-          through: { attributes: [] },
-        },
-      });
-    }
+    } else {
+      let gender;
+      if (genderId) {
+        gender = await Gender.findByPk(genderId, {
+          include: {
+            model: Movie,
+            through: { attributes: [] },
+          },
+        });
+      } else if (genderName) {
+        gender = await Gender.findOne({
+          where: { name: genderName },
+          include: {
+            model: Movie,
+            through: { attributes: [] },
+          },
+        });
+      }
 
-    if (!gender) {
-      return res.status(404).json({ error: "Género no encontrado" });
-    }
+      if (!gender) {
+        return res.status(404).json({ error: "Género no encontrado" });
+      }
 
-    return res.json(gender.Movies);
+      return res.json(gender.Movies);
+    }
   } catch (error) {
     console.error("Error al obtener películas asociadas al género:", error);
     return res.status(500).json({ error: "Error interno del servidor" });
--- routes/movie.js
+++ routes/movie.js
@@ -11,7 +11,7 @@ router.post("/movie", async (req, res) => {
     if (!title || !date_created || !image || !rating || !character) {
       return res.status(400).json({
         error:
-          "Todos los campos (title, date_created, image, rating, character) son requeridos.",
+          `${req.body}Todos los campos (title, date_created, image, rating, character) son requeridos.`,
       });
     }
 
@@ -48,11 +48,14 @@ router.post("/movie", async (req, res) => {
 
 router.get("/movie", async (req, res) => {
   try {
-    const { title, date_created, rating } = req.query;
+    const { title, date_created, rating, gender_id, sort_order } = req.query;
+
+    console.log(gender_id, 'gender_id');
 
     let conditions = [];
+    const includeConditions = [];
 
-    if (!title && !date_created && !rating) {
+    if (!title && !date_created && !rating && !gender_id) {
       Movie.findAll().then((movie) => {
         res.json(movie);
       });
@@ -68,11 +71,29 @@ router.get("/movie", async (req, res) => {
       if (rating) {
         conditions.push({ rating });
       }
+      if (gender_id) {
+        includeConditions.push({
+          model: Gender,
+          where: {
+            id: gender_id,
+          },
+          through: {
+            attributes: []
+          }
+        });
+      }
+
+      let order = [];
+      if (sort_order) {
+        order.push(['date_created', sort_order]);
+      }
 
       const movies = await Movie.findAll({
         where: {
           [Op.and]: conditions,
         },
+        include: includeConditions,
+        order: order,
       });
 
       if (movies.length > 0) {
@@ -84,6 +105,47 @@ router.get("/movie", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).send("Ocurrio un error al realizar la consulta");
+  }
+});
+
+
+router.put("/movie/:id", async (req, res) => {
+  try {
+    const movieId = req.params.id;
+    const updatedData = req.body;
+
+    const movie = await Movie.findByPk(movieId);
+
+    if (!movie) {
+      return res.status(404).json({ message: "Pelicula no encontrada." });
+    }
+    await movie.update(updatedData);
+    res.json(movie);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Ocurrió un error al editar la pelicula." });
+  }
+});
+
+router.delete("/movie/:id", async (req, res) => {
+  try {
+    const movieId = req.params.id;
+
+    const movie = await Movie.findByPk(movieId);
+
+    if (!movie) {
+      return res.status(404).json({ message: "Pelicula no encontrada." });
+    }
+
+    await movie.destroy();
+    res.json({ message: "Pelicula eliminada exitosamente." });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Ocurrió un error al eliminar la pelicula." });
   }
 });
 
